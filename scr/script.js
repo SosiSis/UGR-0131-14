2e6fec7db86e1ebcb33a03e1c3d01becd57868f7
SosiSis <sosinasisay30@gmail.com>
2023-12-22 19:30:15 +0300

    second commit

---
+++ scr/script.js
@@ -0,0 +1,7 @@
+
+//mobile menu
+
+const burger = document.getElementById('burger');
+const menu = document.getElementById('menu');
+
+burger.addEventListener('click',()=>{if(menu.classList.contains('hidden')){menu.classList.remove('hidden')}else{menu.classList.add('hidden')}});
